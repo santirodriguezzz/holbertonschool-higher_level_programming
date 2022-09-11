1b1663c12a4a2f469d6c454dc618e16e27e87f95
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-11 10:41:26 -0700

    task8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if i >= 'a' and i <= 'z':
+            i = chr(ord(i) -32)
+        print("{:s}".format(i), end='')
+    print(end='\n')
