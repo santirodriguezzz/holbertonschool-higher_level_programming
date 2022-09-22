2580f0d627426b14cd6849f07b87118524a76c1c
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-22 18:04:12 -0300

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" create empty
+class square
+"""
+
+
+class Square:
+    """
+    class square
+    """
+    def __init__(self, size=0):
+        """ init is a method for
+        initialize and give to
+        values for an object"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        calculates area of square
+        """
+        area = self.__size * self.__size
+        return area
