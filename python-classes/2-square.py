6011e85c7f97804a01a09ffcb15cc98bd4f2539b
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-22 17:32:04 -0300

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" create empty
+class square
+"""
+
+
+class Square:
+    """
+    class square
+    """
+    def __init__(self, size=0):
+        """ init is a method for
+        initialize and give to
+        values for an object"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0
+        raise TypeError("size must be >= 0")
+        self.__size = size
