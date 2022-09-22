ae709bebc31f2df322d595cbb112c2984490d248
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-22 13:36:57 -0300

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" create empty
+class square
+"""
+
+
+class Square:
+    """
+    class square
+    """
+    def __init__(self, size):
+    """ init is a method for 
+    initialize and give to 
+    values for an object"""
+        
