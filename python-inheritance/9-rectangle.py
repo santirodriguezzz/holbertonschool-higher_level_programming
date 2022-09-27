bba29d77fe09cc5ec18e68ce834e6d633433e392
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-27 19:22:54 -0300

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""python interpreter"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """function"""
+
+    def __init__(self, width, height):
+        """initialize"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self,):
+        return f"[Rectangle] {self.__width} / {self.__height}"
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
