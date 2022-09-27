d41a3b19cc2e1d8f2b6e40ef5fc554c4be1f4af3
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-27 20:14:39 -0300

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""python interpreter"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        """ initialize new instance Square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}" 
+
+    def area(self):
+        """area"""
+        return self.__size * self.__size
