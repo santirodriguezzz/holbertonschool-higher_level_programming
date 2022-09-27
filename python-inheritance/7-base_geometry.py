9c178610aff1170318493291d603d0599db38bc1
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-27 16:28:42 -0300

    task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""python interpreter"""
+
+
+class BaseGeometry():
+    """EmptyCass"""
+    pass
+
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validator"""
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
