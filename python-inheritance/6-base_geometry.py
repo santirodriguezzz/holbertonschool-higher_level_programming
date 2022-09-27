edbeebeece4103364af2587a3b3bf84297734e40
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-27 16:15:44 -0300

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python interpreter"""
+
+
+class BaseGeometry():
+    """EmptyCass"""
+    pass
+
+    def area(self):
+        """area"""
+        raise Exception("area() is not implemented")
