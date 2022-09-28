9391ce191e34bec2cf4768539332418954ab99af
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-28 19:36:22 -0300

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     json_list = []
     filename = 'add_item.json'
     try:
