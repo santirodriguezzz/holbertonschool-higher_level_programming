f43d8357680d1dd296d1d364a373dcd25d418997
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-28 17:00:39 -0300

    pep8

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """function"""
-    with open(filename,'w') as tempFile:
+    with open(filename, 'w') as tempFile:
         tempFile.write(json.dumps(my_obj))
