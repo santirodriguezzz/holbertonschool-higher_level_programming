8bcd22737973ddaa9b3e345ecc8e65401caccfdc
santirodriguezzz <santiagorodri130898@gmail.com>
2022-09-28 16:07:09 -0300

    task 2

--- python-input_output/ 2-append_write.py
+++ python-input_output/ 2-append_write.py
@@ -2,7 +2,7 @@
 """python interpreter"""
 
 
-def write_file(filename="", text=""):
-    """fuction  that writes a string to a text file"""
-    with open(filename, 'a')as tempFile:
+def append_write(filename="", text=""):
+    """fuction  that writes a string to a text files"""
+    with open(filename, 'a') as tempFile:
         return tempFile.write(text)
