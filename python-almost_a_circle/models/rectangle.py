c9440b86d9ecbdde5a5d94862643f9725014f622
santirodriguezzz <santiagorodri130898@gmail.com>
2022-10-02 21:52:29 -0300

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhton3
+"""interpreter"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle"""
+    
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
