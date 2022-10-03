3e29b056fd5c477430e04d3d0463edc496072ab1
santirodriguezzz <santiagorodri130898@gmail.com>
2022-10-03 15:13:51 -0300

    fixxs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,16 +7,15 @@ from models.base import Base
 
 class Rectangle(Base):
     """class Rectangle"""
-    
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """initialize class""" 
+        """initialize class"""
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-       
+
     @property
     def width(self):
         """retrive width"""
