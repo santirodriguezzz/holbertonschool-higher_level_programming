d381cbebbb96d68f61959addda6a5344ba78635f
santirodriguezzz <santiagorodri130898@gmail.com>
2022-10-03 21:51:36 -0300

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""interpreter"""
+
+from models.rectangle import Rectangle
+
+
+    class Square(Rectangle):
+        """class square"""
+
+        def __init__(self, size, x=0, y=0, id=None):
+            """initialize square class"""
+            self.size = size
+            super().__init__(size, size, x, y, id)
+
+        @property
+        def size(self):
+            """retrive size"""
+            return self.__width
+
+        @size.setter
+        def size(self, size):
+            """set size"""
+            self.__width = size
+            self.__height = size
