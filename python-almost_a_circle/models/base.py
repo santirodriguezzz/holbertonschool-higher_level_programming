10565131af962c3e4efec519de2039fbb0460e0e
santirodriguezzz <santiagorodri130898@gmail.com>
2022-10-05 14:21:22 -0300

    tryy

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -44,9 +44,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """that returns the list of the JSON in string"""
-        if json_string is None:
-            return "[]"
-        if len(json_string) == 0:
-            return "[]"
+        if json_string is None or len(json_string) == 0:
+            return []  
         list_Dics = json.loads(json_string)
         return list_Dics
