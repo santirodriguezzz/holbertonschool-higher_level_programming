ad80dbba98dba13a7facae5434f95cce21213006
santirodriguezzz <santiagorodri130898@gmail.com>
2022-10-05 20:43:02 -0300

    tesst

--- python-almost_a_circle/tests/tests_base.py
+++ python-almost_a_circle/tests/tests_base.py
@@ -1,53 +1,15 @@
 #!/usr/bin/python3
 """python interpreter"""
-from models.base import Base
 import unittest
+from models.base import Base
+
 
 
 class basetests1(unittest.TestCase):
     """tests attribute id"""
-    def testsetUp(self):
-        self.b = Base()
-
-    def test_1(self):
-        self.assertEqual(self.b.id, 1)
-
-    def test_2(self):
-        self.assertEqual(self.b.id, 2)
-
-    def test_3(self):
-        self.assertEqual(self.b.id, 3)
-
-class TestBase420(unittest.TestCase):
-    """test for ID assignment"""
-
-    def test_1(self):
-        self.b = Base(89)
-        self.assertEqual(self.b.id, 89)
-
-class TestToJsonString(unittest.TestCase):
-    """tests Base.to_json_string() method"""
 
     def setUp(self):
-        self.b = Base()
+        self.k = Base()
 
-    def test_1(self):
-        self.assertEqual(self.b.to_json_string(None), "[]")
-
-    def test_1(self):
-        self.assertEqual(self.b.to_json_string([1, 2]), "[1, 2]")
-
-class TestFromJsonString(unittest.TestCase):
-    """tests Base.from_json_string"""
-
-    def setUp(self):
-        self.b = Base()
-
-    def test_1(self):
-        self.assertEqual(self.b.from_json_string(None), [])
-
-    def test_1(self):
-        self.assertEqual(self.b.from_json_string("[]"), [])
-
-    def test_1(self):
-        self.assertEqual(self.b.from_json_string('[{ "id": 89 }]'), [{'id': 89}])
+    def test_id(self):
+        self.assertEquals(b, id, 0)
