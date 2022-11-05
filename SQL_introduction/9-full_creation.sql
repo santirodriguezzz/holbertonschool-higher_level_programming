9e7879a448f46590161387c11fa43b77f921a984
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-05 15:28:44 -0300

    task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,27 @@
+-- script that creates a second_table in the database with multiples rows.
+CREATE TABLE IF NOT EXISTS second_table(
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+INSERT INTO
+    `second_table` (`id`, `name`, `score`)
+VALUES
+    (1, "John", 10);
+
+INSERT INTO
+    `second_table` (`id`, `name`, `score`)
+VALUES
+    (2, "Alex", 3);
+
+INSERT INTO
+    `second_table` (`id`, `name`, `score`)
+VALUES
+    (3, "Bob", 14);
+
+INSERT INTO
+    `second_table` (`id`, `name`, `score`)
+VALUES
+    (4, "Geor", 3);
+
