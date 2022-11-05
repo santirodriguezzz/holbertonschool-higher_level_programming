5a319c007386f9604903a8c21e1fa2615105725f
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-05 17:40:37 -0300

    test

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- script that lists the number of records with the same score in the second table
-SELECT score, COUNT(*) AS NUMBER FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
