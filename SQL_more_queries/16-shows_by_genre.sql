9ff939971faccc487d7054f65278d19eda9acdd2
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-06 23:58:17 -0300

    task 16

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT
+    s.title,
+    g.name
+FROM
+    tv_shows s
+    LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+    LEFT JOIN tv_genres g ON g.id = sg.genre_id
+ORDER BY
+    s.title,
+    g.name;
