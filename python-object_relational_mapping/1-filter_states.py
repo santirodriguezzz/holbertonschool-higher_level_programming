50fd7291cb58f1cdc38a7decb698f1ebc5918300
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-08 22:08:16 -0300

    fix 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,10 +2,6 @@
 """
     script that lists all states with a name starting with N from the database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
-
-
 if __name__ == "__main__":
     import MySQLdb
     import sys
