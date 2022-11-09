1f540842f89b0bf07c762100ad8a343718fcf9bb
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-09 14:22:38 -0300

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{}' \ ORDER BY id;".format(sys.argv[4]))
+    result = cursor.fetchall()
+    for record in result:
+        if record[1][0] == sys.argv[4]:
+            print(record)
+    cursor.close()
+    db.close()
