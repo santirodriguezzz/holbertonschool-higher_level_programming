3a59e42bc9897bc1400e6082d5676263cf1cf605
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-09 18:34:42 -0300

    task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that takes in arguments and displays all values in the states"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT c.id, c.name, s.name FROM cities c \ LEFT JOIN states s ON c.state_id = s.id \ ORDER BY c.id")
+    result = cursor.fetchall()
+    for record in result:
+        print(record)
+    cursor.close()
+    db.close()
