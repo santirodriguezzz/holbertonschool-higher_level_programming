c74076237048a08dab4a954ccabb942f8caf8fe2
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-08 22:44:09 -0300

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""script that lists all states with a name starting with N"""
 import MySQLdb
 import sys
 
