5a2787c8d6ea18b424006467310951d855b0a791
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-08 22:13:17 -0300

    test

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-    script that lists all states with a name starting with N from the database hbtn_0e_0_usa
-"""
+"""script that lists all states with a name starting with N from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
