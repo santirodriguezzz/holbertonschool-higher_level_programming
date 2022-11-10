7991f33980cbf90a8657a5f3b987e5d9761f547e
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-10 15:41:37 -0300

    task11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" script that adds the State object Louisiana"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
+
+Base = declarative_base()
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State()
+    new_state.name = 'Louisiana'
+    session.add(new_state)
+    result = session.query(State).filter(
+        State.name.like(new_state.name)).first()
+    print(result.id)
+
+    session.commit()
+    session.close()
