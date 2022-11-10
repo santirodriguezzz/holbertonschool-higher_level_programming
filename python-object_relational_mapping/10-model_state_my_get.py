a8b38b39d90f18048bedf7220065ec3507186160
santirodriguezzz <santiagorodri130898@gmail.com>
2022-11-10 15:33:12 -0300

    task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that prints the State object with the name passed"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
+
+Base = declarative_base()
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(
+        State.name.like(argv[4])).first()
+    if result is not None:
+        print(f'{result.id}')
+    else:
+        print('Not found')
+    session.close()
