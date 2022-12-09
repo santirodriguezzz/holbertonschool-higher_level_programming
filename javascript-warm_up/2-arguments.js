9bc48b0ed3042751386ceac8e7ae7c067aefb81e
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-09 15:19:11 -0300

    task 2 fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-if (process.argv.length === 0) {
+if (process.argv.length < 3) {
   console.log('No argument');
-}
-
-if (process.argv.length === 1) {
-  console.log('Argument found');
 }
 
 if (process.argv.length === 3) {
   console.log('Argument found');
+}
+
+if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
