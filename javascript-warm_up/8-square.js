a6e8bebacf0d607af82da7fc91645682a8c653f4
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-10 17:46:39 -0300

    fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const size = process.argv[2];
-if (isNaN(size)){
-    console.log("Missing size")
-    return;
+if (isNaN(size)) {
+  console.log('Missing size');
 }
+
 for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let x = 0; x < size; x++) {
-        row += "X";
-    }
-    console.log(row);
+  let row = '';
+  for (let x = 0; x < size; x++) {
+    row += 'X';
+  }
+  console.log(row);
 }
