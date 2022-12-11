05ce026ebc7774845e2c61f6105f7d7abef8514a
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-11 15:09:02 -0300

    fix semii

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const result = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
 console.log(result);
-function add (a, b){
-    return a + b;
-};
+function add (a, b) {
+  return a + b;
+}
