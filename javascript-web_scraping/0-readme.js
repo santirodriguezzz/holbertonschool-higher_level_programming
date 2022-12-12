a0a76c35ec8c2567868ef0ad5ce22f134ff1ab31
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-12 19:12:47 -0300

    fix

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const  path = process.argv[2];
+const path = process.argv[2];
 request.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
