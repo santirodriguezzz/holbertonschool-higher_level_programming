20afa000ce6b8742dfb61a7ae6bbce5adce91408
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-13 19:41:42 -0300

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-const path = process.argv[2];
-request.readFile(path, 'utf8', (err, data) => {
+const path = process.argv;
+
+request.readFile(path[2], 'utf8', (err, data) => {
   if (err) {
     return console.log(err);
   } else {
