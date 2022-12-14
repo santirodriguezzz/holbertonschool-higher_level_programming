74bc0ea6d4b17e1b9665092d488d25a53305eff3
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-13 21:59:08 -0300

    test

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
-const path = process.argv;
+const fs = require('fs');
+const args = process.argv;
 
-request.readFile(path[2], 'utf8', (err, data) => {
+fs.readFile(args[2], 'utf8', (err, data) => {
   if (err) {
     return console.log(err);
   }
