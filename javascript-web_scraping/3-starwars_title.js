bb4ef429365659abf7cf9e582be34821063f54c4
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-16 13:34:08 -0300

    test2

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,22 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
+const id = process.argv[2];
 
-request.get(url, function (err, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + id, function (err, response, body) {
   if (err) {
-    throw err;
+    return console.log(err);
   } else {
     if (response.statusCode === 200) {
-      const data = JSON.parse(body).results;
-      let count = 0;
-      for (const each of data) {
-        for (const chars of each.characters) {
-          if (chars.endsWith('/18/')) {
-            ++count;
-          }
-        }
-      }
-      return console.log(count);
+      console.log(JSON.parse(body).title);
     }
   }
 });
