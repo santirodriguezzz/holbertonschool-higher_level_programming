43fbe97670c14378a0aeda7949267aa60bc69fb9
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-17 14:18:11 -0300

    task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+const rq = require('fs');
+const args = process.argv;
+let web = 'a';
+
+request(args[2], function (error, response, body) {
+    if (error){
+        console.log(error);
+    }
+    web = body;
+    fs.writeFile(args[3], web, 'utf-8', (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+});
