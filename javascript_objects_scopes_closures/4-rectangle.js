19218c168377d34fefe591891e69647ad903bbd3
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-30 17:04:19 -0300

    task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  // module.exportsdeclaración está exportando una clase llamada Rectangle
+
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+      // El constructor establece los valores de estos argumentos como propiedades de la Rectangle instancia mediante la thispalabra clave
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
