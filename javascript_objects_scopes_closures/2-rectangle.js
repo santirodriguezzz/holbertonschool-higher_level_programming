f3f346c98a3a9cd718efb6b8bf559a1d743faba1
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-30 14:43:34 -0300

    fixxs

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   // module.exportsdeclaración está exportando una clase llamada Rectangle
   constructor (w, h) {
-    if (w > 0 || h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
       // El constructor establece los valores de estos argumentos como propiedades de la Rectangle instancia mediante la thispalabra clave
