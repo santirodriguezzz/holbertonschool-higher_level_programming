b5e6cc107e515b9d1b9a4865f66f3f4bdfddf506
santirodriguezzz <santiagorodri130898@gmail.com>
2023-01-01 15:24:20 -0300

    fixs

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    function conversor (item) {
-      return item.toString(base);
-    }
-    return conversor;
-  };
+  function conversor (item) {
+    return item.toString(base);
+  }
+  return conversor;
+};
