1d0f705ffdb7ea1eca6f1e7dcd5eee4d9a7838f4
santirodriguezzz <santiago22rodriguezz@gmailcom>
2023-05-12 19:19:43 -0300

    try2

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 """ claso of rectangle """
 class Rectangle:
-        def __init__(self, width, height):
-            self.width = width
-            self.height = height
+        def __init__():
